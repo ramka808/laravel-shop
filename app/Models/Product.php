f21da56d1cd27e7eeb9a01ab352edb72aa6cf3dd
ramil <ramka808>
2023-11-25 05:21:28 +0300

    edit models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * Возвращает категорию выбранного товара
+     */
+    /**
+     * Связь «товар принадлежит» таблицы `products` с таблицей `categories`
+     */
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Связь «товар принадлежит» таблицы `products` с таблицей `brands`
+     */
+    public function brand() {
+        return $this->belongsTo(Brand::class);
+    }
 }
